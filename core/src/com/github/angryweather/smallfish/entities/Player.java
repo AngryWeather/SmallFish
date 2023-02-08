385af3360f425a3446fc3cf833bad528f50179c2
Weronika <WerRic96@protonmail.com>
2023-02-09 00:18:26 +0100

    Create player class

---
+++ core/src/com/github/angryweather/smallfish/entities/Player.java
@@ -0,0 +1,16 @@
+package com.github.angryweather.smallfish.entities;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+import com.github.angryweather.smallfish.SmallFish;
+
+public class Player {
+    private final Rectangle playerRect = new Rectangle();
+
+    public Player(TextureRegion player) {
+        playerRect.x = 0;
+        playerRect.y = SmallFish.HEIGHT / 2f - (player.getRegionHeight() / 2f);
+        playerRect.width = player.getRegionWidth();
+        playerRect.height = player.getRegionHeight();
+    }
+}
